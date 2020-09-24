0cd819f97c305df4f46076bd8e4962c3dbe43d11
duynguyen158 <57234880+duynguyen158@users.noreply.github.com>
2020-09-24 11:11:48 +0700

    Add articles

--- src/sections/Articles.js
+++ src/sections/Articles.js
@@ -1,12 +1,67 @@
-import React from 'react';
-//import 
+import React, { useState } from 'react';
+import { utcParse } from 'd3-time-format';
+import Text from './Text';
+import linkWrap from './utilities/linkWrap';
 
 function Articles({ data }) {
+    const examples = data.examples;
+    const cells = examples.map((d, i) => <Cell key={i} data={d} />)
     return (
-        <div>
-            TBD
+        <div className="wrap">
+            {cells}
         </div>
     );
 } 
 
 export default Articles;
+
+function Cell({ data }) {
+    // Hook to store and get URL of thumbnail
+    const [ thumbnailUrl, setThumbnailUrl ] = useState(null);
+
+    const {
+        id, title, date, publication, 
+        url, description, extraStyle
+    } = data;
+
+    // Fetch thumbnail
+    import(`../media/images/${id}.jpg`).then(image => {
+        setThumbnailUrl(image.default);
+    });
+
+    const style = {
+        marginBottom: '1em',
+        borderRadius: '3px'
+    };
+    const image = (
+        <img  
+            src={thumbnailUrl} 
+            width="100%"
+            style={Object.assign(style, extraStyle)}
+        />
+    );
+
+    return (
+        <div id={id} className="cell">
+            {linkWrap(image, url)}
+            <div className="info">
+                {linkWrap(<h3>{title}</h3>, url)}
+                <p className="publication">{publication}</p>
+                <small className="time">{toDateString(parseDate(date))}</small>
+            </div>
+            <Text data={description} classNames={['description']} />
+        </div>
+    );
+}
+
+// ------------HELPERS-------------
+const parseDate = utcParse('%m/%d/%y');
+
+function toDateString(date) {
+    return date.toLocaleString(undefined, {
+        year: "numeric",
+        month: "short",
+        day: "numeric",
+        timeZone: 'UTC'
+    })
+}
